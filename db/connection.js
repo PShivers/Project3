bfe7d0850ba8e99a3d26d55786a01107a7ebb7ac
Paul Shivers <paulsshivers@gmail.com>
2019-05-14 10:02:11 -0400

    added connection.js/.env

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost/bog1");
+}
+
+mongoose.connection.on("error", error => {
+  console.error(`MongoDB connection error!!! ${error}`);
+  process.exit(-1);
+});
+
+mongoose.connection.once("open", () => {
+  console.log(`Mongoose has connected to MongoDB`);
+});
+module.exports = mongoose;
